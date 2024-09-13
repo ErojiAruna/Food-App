3afc4f972e6157e1ed996afcc6c376f33a8c8eeb
ErojiAruna <aruna.eroji@gamil.com>
2024-09-13 17:45:36 +0530

    episode - 03

--- App.js
+++ App.js
@@ -1,40 +1,20 @@
-/**
- * <div id="parent">
- *     <div id="child">
- *        <h1>I'm h1 tag</h1>
- *        <h2>I'm h2 tag</h2>
- *     </div>
- * </div>
- *
- *ReactElement(Object)
- */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, "I'm an h1 tag"),
-    React.createElement('h2', {}, "I'm an h2 tag"),
-  ])
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
+);
+
+// Component composition
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(parent);
-
-const parent1 = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement('div', { id: 'child1' }, [
-    React.createElement('h1', {}, 'Heading1'),
-    React.createElement('h2', {}, 'Heading 2'),
-  ]),
-  React.createElement('div', { id: 'child2' }, [
-    React.createElement('h1', {}, 'Heading 1'),
-    React.createElement('h2', {}, 'Heading 2'),
-  ])
-);
-
-const root1 = ReactDOM.createRoot(document.getElementById('root'));
-
-root1.render(parent1);
+root.render(<HeadingComponent />);
